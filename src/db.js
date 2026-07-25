b7270c1187a3b69a1439d518e468aecb983c8147
mateorapt23 <msrodriguezca@puce.edu.ec>
2026-07-25 05:33:42 -0500

    Update db.js

--- src/db.js
+++ src/db.js
@@ -7,6 +7,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
 });
 
 // Crea la tabla si no existe, apenas arranca la app
